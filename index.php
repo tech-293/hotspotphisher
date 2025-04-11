4675cf9386484316be431e9a8740ac7441a91ddf
tech-293 <manjiephraim1@gmail.com>
2025-04-11 03:55:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'ip.php';?>
 <script type="text/javascript">
 if (screen.width <= 699) {
-    document.location = "mobile.html";
+    document.location = "index.html";
 }
 else {
     document.location = "login.html";
